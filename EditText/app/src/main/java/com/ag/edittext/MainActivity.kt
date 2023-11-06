a8f304f786eab8f6b7c1cd338c79497991785dd2
YeaAg <yegkwkstella@gamil.com>
2023-11-06 20:17:07 +0900

    EditText

---
+++ EditText/app/src/main/java/com/ag/edittext/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ag.edittext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import com.ag.edittext.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        with(binding){
+            editText.addTextChangedListener { editable ->
+                //Log.d("에딧텍스트","입력된값${editable.toString()}")
+                textView.text = editable.toString()
+            }
+        }
+    }
+}
